d01fe79b4d08af4cd91dffa37774e77dcc9afacf
BuildTools <unconfigured@null.spigotmc.org>
2025-05-05 18:57:37 -0600

    new color

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const config = {
     apiversion: "1",
     author: "💔💔",  // TODO: Your Battlesnake Username
-    color: "#FF22FF", // TODO: Choose color
+    color: "#AC50AC", // TODO: Choose color
     head: "trans-rights-scarf",  // TODO: Choose head, see https://play.battlesnake.com/customizations/ for options unlocked in your account
     tail: "replit-notmark"  // TODO: Choose tail, see https://play.battlesnake.com/customizations/ for options unlocked in your account
 }
